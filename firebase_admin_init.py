096a07eb6f766d51efe62a7aff859deaa661b7c2
sainathhk <sainathh.k306@gmail.com>
2026-01-19 23:41:35 +0530

    update

--- firebase_admin_init.py
+++ firebase_admin_init.py
@@ -1,4 +1,4 @@
-import firebase_admin
+'''import firebase_admin
 from firebase_admin import credentials, auth, firestore
 
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -6,3 +6,14 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
+'''
+
+import os, json
+from firebase_admin import credentials, initialize_app
+
+service_account_info = json.loads(
+    os.environ.get("FIREBASE_SERVICE_ACCOUNT")
+)
+
+cred = credentials.Certificate(service_account_info)
+initialize_app(cred)
